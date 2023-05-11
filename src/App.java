736fc48fc94e906a2ea5ad42843533d3e7111bad
Sulaiman Hashim <93908604+sulaimanmhashim@users.noreply.github.com>
2023-05-12 02:46:06 +0800

    Initial Commit For Milagro Man
    - Added class MilagroMan.java
    - Added basic constructor
    - Added menu but not fully functional yet
    - Should extend MoodyBlues. and/or PearlJam when they are ready.

---
+++ src/App.java
@@ -0,0 +1,9 @@
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        MilagroMan mg = new MilagroMan("Rest 1 Maju");
+        mg.Menu();
+    }
+}
