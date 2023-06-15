fdde6d138ad4497a315d423230e49a9efba324dd
Sulaiman Hashim <93908604+sulaimanmhashim@users.noreply.github.com>
2023-06-15 14:14:32 +0800

    test

--- src/App.java
+++ src/App.java
@@ -1,38 +1,9 @@
-import java.util.Scanner;
-
 import defaultmap.Defaultmap;
-import javafx.application.Application;
-import javafx.stage.Stage;
-import ui.MainController;
 
-public class App extends Application {
+public class App {
     public static void main(String[] args) throws Exception {
-        /*System.out.println("Hello, World!\nENTER 1 FOR GUI, 2 FOR CLI:");
-        // MoodyBlues.callMenu();
-        // Restaurant.InitializeRestaurant();
-        // System.out.println("\nEnter restaurant name:");
-        Scanner scanner = new Scanner(System.in);
-        String string = /*"1";scanner.nextLine();
-        switch (string) {
-            case "1":
-                launch(args);
-                break;
-            case "2":*/
-                Defaultmap.start();
-                /*break;
-            default:
-                System.out.println("wtf?");
-                System.exit(0);
-                break;
-        }*/
-        // MilagroMan mg = new MilagroMan(string);
-        // mg.Menu();
-        // scanner.close();*/
-    }
 
-    @Override
-    public void start(Stage arg0) throws Exception {
-        MainController ui = new MainController();
-        ui.start(arg0);
+        Defaultmap.start();
+
     }
 }
